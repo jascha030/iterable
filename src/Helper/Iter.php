0885c5f1b051118bef15a2c848d5263df471e2cd
Jascha030 <contact@jaschavanaalst.nl>
2022-12-27 23:45:26 +0100

    ✨ Add Iter::makeRewindable(generator_fn) helper method.

--- src/Helper/Iter.php
+++ src/Helper/Iter.php
@@ -6,6 +6,7 @@ namespace Jascha030\Iterable\Helper;
 
 use Closure;
 use Generator;
+use Jascha030\Iterable\Rewindable\RewindableGenerator;
 use ReflectionFunction;
 
 final class Iter
@@ -15,12 +16,12 @@ final class Iter
      */
     public static function reduce(callable|Closure $callback, iterable $iterable, mixed $startValue = null): mixed
     {
-        $accumulator = $startValue;
-        $paramCount  = (new ReflectionFunction($callback))->getNumberOfParameters();
+        $accumulator        = $startValue;
+        $numberOfParameters = (new ReflectionFunction($callback))->getNumberOfParameters();
 
         foreach ($iterable as $key => $value) {
             $accumulator = $callback(
-                ...(static fn(): Generator => match ($paramCount) {
+                ...(static fn(): Generator => match ($numberOfParameters) {
                     2 => yield from [$accumulator, $value],
                     3 => yield from [$accumulator, $value, $key]
                 })()
@@ -28,5 +29,10 @@ final class Iter
         }
 
         return $accumulator;
+    }
+
+    public static function makeRewindable(callable|Closure $function): Closure
+    {
+        return RewindableGenerator::from($function);
     }
 }
