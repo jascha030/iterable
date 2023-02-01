35e361da78bb22c02f87be5d36218aa2f9a83fd7
Jascha030 <contact@jaschavanaalst.nl>
2023-02-01 15:00:28 +0100

    🎨 Cleanup

--- src/Helper/Iter.php
+++ src/Helper/Iter.php
@@ -7,23 +7,31 @@ namespace Jascha030\Iterable\Helper;
 use Closure;
 use Generator;
 use Jascha030\Iterable\Rewindable\RewindableGenerator;
+use ReflectionException;
 use ReflectionFunction;
 
 final class Iter
 {
     /**
-     * @throws \ReflectionException
+     * @param Closure $callback
+     * @param array|mixed[]  $iterable
+     * @param null|mixed     $startValue
+     *
+     * @return mixed
+     * @throws ReflectionException
+     * @noinspection PhpPluralMixedCanBeReplacedWithArrayInspection
      */
-    public static function reduce(callable|Closure $callback, iterable $iterable, mixed $startValue = null): mixed
+    public static function reduce(Closure $callback, iterable $iterable, mixed $startValue = null): mixed
     {
         $accumulator        = $startValue;
         $numberOfParameters = (new ReflectionFunction($callback))->getNumberOfParameters();
 
         foreach ($iterable as $key => $value) {
             $accumulator = $callback(
-                ...(static fn(): Generator => match ($numberOfParameters) {
+                ...(static fn (): Generator => match ($numberOfParameters) {
                     2 => yield from [$accumulator, $value],
-                    3 => yield from [$accumulator, $value, $key]
+                    3 => yield from [$accumulator, $value, $key],
+                    default => throw new \ArgumentCountError('$callback should have 2 or 3 parameters.')
                 })()
             );
         }
@@ -31,6 +39,11 @@ final class Iter
         return $accumulator;
     }
 
+    /**
+     * @param callable|Closure $function
+     *
+     * @return Closure
+     */
     public static function makeRewindable(callable|Closure $function): Closure
     {
         return RewindableGenerator::from($function);
