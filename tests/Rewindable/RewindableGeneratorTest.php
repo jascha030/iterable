75730606a5008519ad2c38a289ee96362e6abda2
Jascha030 <contact@jaschavanaalst.nl>
2023-02-01 15:01:29 +0100

    ✨ Type-safe RewindableGenerator class.

--- tests/Rewindable/RewindableGeneratorTest.php
+++ tests/Rewindable/RewindableGeneratorTest.php
@@ -1,10 +1,43 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Jascha030\Iterable\Rewindable;
 
 use PHPUnit\Framework\TestCase;
 
-class RewindableGeneratorTest extends TestCase
+final class RewindableGeneratorTest extends TestCase
 {
+    public function testCurrent(): void
+    {
+    }
 
+    public function testFrom(): void
+    {
+        self::assertInstanceOf(\Iterator::class, $);
+    }
+
+    public function testKey(): void
+    {
+    }
+
+    public function testNext(): void
+    {
+    }
+
+    public function testRewind(): void
+    {
+    }
+
+    public function testSend(): void
+    {
+    }
+
+    public function testThrow(): void
+    {
+    }
+
+    public function testValid(): void
+    {
+    }
 }
